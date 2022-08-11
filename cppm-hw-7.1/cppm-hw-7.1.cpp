1e9bf15b2293c789a998327b6152bf21ce4c7c72
MaxM76 <m.v.markelov@mail.ru>
2022-08-11 08:27:07 +0300

    Initial commit

---
+++ cppm-hw-7.1/cppm-hw-7.1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#define MODE 0
+#ifndef MODE
+#error необходимо определить MODE
+
+#else
+
+#if MODE == 0
+#define REGIMEMSG "Работаю в режиме тренировки"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << REGIMEMSG << "\n";
+}
+
+#elif MODE == 1
+
+#define REGIMEMSG "Работаю в боевом режиме"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << REGIMEMSG << "\n";
+    int a;
+    int b;
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    std::cout << "Результат сложения: " << add(a, b) << "\n";
+}
+
+#else
+
+#define REGIMEMSG "Неизвестный режим. Завершение работы"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << REGIMEMSG << "\n";
+}
+
+#endif
+
+#endif
